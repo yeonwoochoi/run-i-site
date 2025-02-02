c8a4cf71b861054f721cb1afe0a84d23570a6143
choiyeonwoo <chldusdn20@gmail.com>
2025-02-03 00:43:53 +0900

    clean: remove unused code and imports
    - Removed unused imports for `defineEmits` and other unnecessary code
    - Cleaned up unused variables and functions

--- src/components/three/objects/cube.js
+++
@@ -1,8 +0,0 @@
-import { BoxGeometry, Mesh } from 'three';
-import { createStandardMaterial } from "../materials/standard-material.js";
-
-export function createCube() {
-  const geometry = new BoxGeometry(1, 1, 1);
-  const cube = new Mesh(geometry, createStandardMaterial());
-  return cube;
-}
