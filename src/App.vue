5ba24f18f941104018a52fc051f458bc31877a2b
beagle <chldusdn20@gmail.com>
2025-02-04 14:51:26 +0900

    feat: add loading screen

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="flex flex-col justify-between min-h-screen">
+  <LoadingScreen v-show='loading' />
+  <div class='flex flex-col justify-between min-h-screen'>
     <TheHeader />
     <div class="flex-grow"> <!-- Main content area -->
       <TheView />
@@ -8,10 +9,28 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import TheHeader from '@/layouts/TheHeader.vue'
-import TheView from '@/layouts/TheView.vue'
-import TheFooter from '@/layouts/TheFooter.vue'
+<script setup>
+import TheHeader from '@/layouts/TheHeader.vue';
+import TheView from '@/layouts/TheView.vue';
+import TheFooter from '@/layouts/TheFooter.vue';
+import LoadingScreen from './components/common/LoadingScreen.vue';
+import FadeTransition from '@/components/common/FadeTransition.vue';
+import { ref, onMounted, provide, nextTick } from 'vue'
+
+const loading = ref(true);
+const fadeTransitionRef = ref(null);
+
+onMounted(() => {
+  nextTick(() => {
+    fadeTransitionRef.value?.startTransition(() => {
+      loading.value = false;
+    });
+  })
+});
+
+provide('useTransition', (callback) => {
+  fadeTransitionRef.value?.startTransition(callback);
+});
 </script>
 
 <style scoped>
---
+++ src/components/common/LoadingScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 초기 로딩 화면 -->
+  <div class="fixed inset-0 z-[9999] flex items-center justify-center bg-black">
+    <div class="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-white"></div>
+  </div>
+</template>
+
+<style scoped>
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.animate-spin {
+  animation: spin 1s linear infinite;
+}
+</style>
